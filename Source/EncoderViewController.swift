326fcf1451cfa1a97f175eab8fc9c8e726c5addb
Nicolas Degen <nicolas@kapanu.com>
2018-09-05 18:08:49 +0200

    different buttons

--- Source/EncoderViewController.swift
+++ Source/EncoderViewController.swift
@@ -12,18 +12,35 @@ import AVKit
 class EncoderViewController: UIViewController {
   var textEntry: UITextView!
   var morseButton: UIButton!
+  var resetButton: UIButton!
   
   let flashlightEncoder = FlashlightEncoder()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     #if DEBUG
-//    flashlightEncoder?.emitMorseMessage("SMS and you")
+    // flashlightEncoder?.emitMorseMessage("SMS and you")
     #endif
   }
   
-  @objc func morseTextField() {
+  @objc func morseTextField(_ sender: UIButton?) {
     flashlightEncoder?.emitMorseMessage(textEntry.text)
+    sender?.backgroundColor = .red
+    morseButton.setTitle("Cancel", for: .normal)
+    sender?.removeTarget(nil, action: nil, for: .allEvents)
+    morseButton.addTarget(self, action: #selector(cancelMorsing), for: .touchDown)
+  }
+  
+  @objc func cancelMorsing(_ sender: UIButton?) {
+    // TODO: cancel morsing
+    sender?.backgroundColor = .green
+    morseButton.setTitle("Morse", for: .normal)
+    sender?.removeTarget(nil, action: nil, for: .allEvents)
+    morseButton.addTarget(self, action: #selector(morseTextField), for: .touchDown)
+  }
+  
+  @objc func resetTextField() {
+    textEntry.text = ""
   }
   
   override func loadView() {
@@ -36,15 +53,27 @@ class EncoderViewController: UIViewController {
     
     morseButton = UIButton(frame: .zero)
     view.addSubview(morseButton)
-    morseButton.backgroundColor = .black
+    morseButton.backgroundColor = .green
     morseButton.layer.cornerRadius = 5
     morseButton.setTitle("Morse", for: .normal)
     morseButton.addTarget(self, action: #selector(morseTextField), for: .touchDown)
     morseButton.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([morseButton.heightAnchor.constraint(equalToConstant: 60),
-                                 morseButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-                                 morseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                                 morseButton.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -20)])
+                                 morseButton.leftAnchor.constraint(equalTo: margins.leftAnchor),
+                                 morseButton.rightAnchor.constraint(equalTo: margins.centerXAnchor, constant: -10),
+                                 morseButton.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -10)])
+    
+    resetButton = UIButton(frame: .zero)
+    view.addSubview(resetButton)
+    resetButton.backgroundColor = .blue
+    resetButton.layer.cornerRadius = 5
+    resetButton.setTitle("Reset", for: .normal)
+    resetButton.addTarget(self, action: #selector(resetTextField), for: .touchDown)
+    resetButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([resetButton.heightAnchor.constraint(equalToConstant: 60),
+                                 resetButton.rightAnchor.constraint(equalTo: margins.rightAnchor),
+                                 resetButton.leftAnchor.constraint(equalTo: margins.centerXAnchor, constant: 10),
+                                 resetButton.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -10)])
     
     textEntry = UITextView(frame: .zero)
     textEntry.backgroundColor = UIColor.white.withAlphaComponent(0.2)
@@ -63,11 +92,12 @@ class EncoderViewController: UIViewController {
     
     view.addSubview(textEntry)
     textEntry.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([textEntry.bottomAnchor.constraint(equalTo: morseButton.topAnchor, constant: -20),
-                                 textEntry.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-                                 textEntry.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                                 textEntry.topAnchor.constraint(equalTo: margins.topAnchor, constant: 20)])
+    NSLayoutConstraint.activate([textEntry.bottomAnchor.constraint(equalTo: morseButton.topAnchor, constant: -10),
+                                 textEntry.leftAnchor.constraint(equalTo: margins.leftAnchor),
+                                 textEntry.rightAnchor.constraint(equalTo: margins.rightAnchor),
+                                 textEntry.topAnchor.constraint(equalTo: margins.topAnchor, constant: 10)])
   }
+  
   
   @objc func dismissKeyboard() {
     view.endEditing(true)
